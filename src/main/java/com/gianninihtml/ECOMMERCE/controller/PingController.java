2124a37babcf4db4b33b179fc4c610994bd97199
Gustavo Giannini <gustavo-giannini@hotmail.com>
2023-09-30 16:35:37 -0300

    ping

---
+++ src/main/java/com/gianninihtml/ECOMMERCE/controller/PingController.java
@@ -0,0 +1,20 @@
+package com.gianninihtml.ECOMMERCE.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PingController {
+	
+	@Value("${mensagem}")
+	private String mensagem;
+
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ping pong: " + mensagem;
+    }
+}
